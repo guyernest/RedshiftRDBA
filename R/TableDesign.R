09f8881e79c451ea289b56e1a2edbbae552c83e6
Guy Ernest <gernest@amazon.com>
2014-02-18 09:46:28 +0100

    Wrapping future queries to avoid Source exeptions

--- R/TableDesign.R
+++ R/TableDesign.R
@@ -32,38 +32,40 @@ plotTable <- function(summaryTable) {
   return (tile)
 }
 
-
-#This query is getting the amount of storage each column in the table is using
-# It is adding as the last 3 columns system columns for (INSERT_XID, DELETE_XID, and ROW_ID (OID))
-select trim(relname) as table_name, col, count(*) as slices
-from stv_blocklist s, pg_class p
-where s.tbl=p.oid and relname = 'cf_logs'
-group by 1,2
-order by 1,2;
-
-# This query is getting the user table schema, name, oid and number of blocks used
-SELECT trim(n.nspname), trim(c.relname), c.oid, 
-(SELECT COUNT(*) FROM STV_BLOCKLIST b WHERE b.tbl = c.oid)
-FROM pg_namespace n, pg_class c
-WHERE n.oid = c.relnamespace 
-AND nspname NOT IN ('pg_catalog', 'pg_toast', 'information_schema')
-
-#This query is getting the information of a table
-select * from PG_TABLE_DEF where tablename='cf_logs';
-
-# This query is getting the columns of a table in order with their data type
-select column_name, ordinal_position, data_type from information_schema.columns where
-table_name='cf_logs'
-order by 2
-
-
-# This query is getting the number of rows that are each slice of the cluster for each table
-
-select trim(name) as table, stv_blocklist.slice, stv_tbl_perm.rows
-from stv_blocklist,stv_tbl_perm
-where stv_blocklist.tbl=stv_tbl_perm.id
-and stv_tbl_perm.slice=stv_blocklist.slice
-and stv_tbl_perm.id > 10000 and name not like '%#m%'
-and name not like 'systable%'
-group by name, stv_blocklist.slice, stv_tbl_perm.rows
-order by 3 desc;
+future_queries_to_use <- function() {
+  #This query is getting the amount of storage each column in the table is using
+  # It is adding as the last 3 columns system columns for (INSERT_XID, DELETE_XID, and ROW_ID (OID))
+  storageAmount <- dbGetQuery(con, "select trim(relname) as table_name, col, count(*) as slices
+  from stv_blocklist s, pg_class p
+  where s.tbl=p.oid and relname = 'cf_logs'
+  group by 1,2
+  order by 1,2;")
+  
+  # This query is getting the user table schema, name, oid and number of blocks used
+  blockNumber <- dbGetQuery(con, "SELECT trim(n.nspname), trim(c.relname), c.oid, 
+  (SELECT COUNT(*) FROM STV_BLOCKLIST b WHERE b.tbl = c.oid)
+  FROM pg_namespace n, pg_class c
+  WHERE n.oid = c.relnamespace 
+  AND nspname NOT IN ('pg_catalog', 'pg_toast', 'information_schema')")
+  
+  #This query is getting the information of a table
+  #select * from PG_TABLE_DEF where tablename='cf_logs';
+  
+  # This query is getting the columns of a table in order with their data type
+  #select column_name, ordinal_position, data_type from information_schema.columns where
+  #table_name='cf_logs'
+  #order by 2
+  
+  
+  # This query is getting the number of rows that are each slice of the cluster for each table
+  
+  rowNumber <- dbGetQuery(con, "select trim(name) as table, stv_blocklist.slice, stv_tbl_perm.rows
+  from stv_blocklist,stv_tbl_perm
+  where stv_blocklist.tbl=stv_tbl_perm.id
+  and stv_tbl_perm.slice=stv_blocklist.slice
+  and stv_tbl_perm.id > 10000 and name not like '%#m%'
+  and name not like 'systable%'
+  group by name, stv_blocklist.slice, stv_tbl_perm.rows
+  order by 3 desc;")
+  
+}
